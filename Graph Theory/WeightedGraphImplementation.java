7e2e49b3c579bf6a7a24107c6aec5692135326ad
JAY DANGAR <jayjaydangar96@gmail.com>
2020-05-16 08:52:23 +0530

    Adding BFS

--- Graph Theory/WeightedGraphImplementation.java
+++ Graph Theory/WeightedGraphImplementation.java
@@ -1,24 +1,27 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
+
 
 public class WeightedGraphImplementation {
 
-    static void insert(Map<Integer,Vertex> graph,int vertexValue,int connectedVertexValue){
+    static void insert(Map<Integer, Vertex> graph, int vertexValue, int connectedVertexValue) {
         Vertex vertex;
         ArrayList<Vertex> connectedNodes;
         Vertex connectedVertex = new Vertex(connectedVertexValue);
-        
-        if(!graph.containsKey(connectedVertexValue)){
+
+        if (!graph.containsKey(connectedVertexValue)) {
             graph.put(connectedVertexValue, connectedVertex);
         }
 
-        if(graph.containsKey(vertexValue)){
+        if (graph.containsKey(vertexValue)) {
             vertex = graph.get(vertexValue);
             connectedNodes = vertex.getConnectedNodes();
             connectedNodes.add(connectedVertex);
-            graph.put(vertexValue,vertex);
+            graph.put(vertexValue, vertex);
             return;
         }
 
@@ -29,37 +32,62 @@ public class WeightedGraphImplementation {
     }
 
     public static void main(String[] args) {
-        
-        Map<Integer,Vertex> graph = new HashMap<Integer,Vertex>();
-        insert(graph, 0, 1);
-        // insert(graph, 1, 2);
-        insert(graph, 2, 3);
-        insert(graph, 3, 4);
-        insert(graph, 3, 1);
-        insert(graph, 4, 0);
-        insert(graph, 4, 1);
-        insert(graph, 7, 2);
 
-        // insert(graph, 0, 5);
-        // insert(graph, 5, 3);
-        // insert(graph, 5, 7);
+        Map<Integer, Vertex> graph = new HashMap<Integer, Vertex>();
+        // insert(graph, 0, 1);
+        // insert(graph, 1, 2);
+        // insert(graph, 1, 7);
+        // insert(graph, 2, 3);
+        // insert(graph, 3, 4);
+        // insert(graph, 3, 1);
+        // insert(graph, 4, 0);
+        // insert(graph, 4, 1);
+        // insert(graph, 7, 2);
+
+        insert(graph, 0, 5);
+        insert(graph, 5, 0);
+        insert(graph, 5, 3);
+        insert(graph, 3, 5);
+        insert(graph, 5, 7);
 
         // printGraph(graph);
-        printDFS(graph);
+        // printDFS(graph);
+        printBFS(graph);
+    }
+
+    private static void printBFS(Map<Integer, Vertex> graph) {
+        int startIndex = 3;
+        printBFSHelper(graph, startIndex);
+    }
+
+    private static void printBFSHelper(Map<Integer, Vertex> graph, int startIndex) {
+        Queue<Vertex> queue = new LinkedList<>();
+        queue.add(graph.get(startIndex));
+        while (!queue.isEmpty()) {
+            Vertex vertex = queue.poll();
+            if (!vertex.isTraversed()) {
+                System.out.print(vertex.getValue() + " ");
+                vertex.setTraversed(true);
+                ArrayList<Vertex> connectedNodes = vertex.getConnectedNodes();
+                for (Vertex connectedNode : connectedNodes) {
+                    queue.add(graph.get(connectedNode.getValue()));
+                }
+            }
+        }
     }
 
     private static void printDFS(Map<Integer, Vertex> graph) {
-        printDFSHelper(graph,4);    
- 	}
+        printDFSHelper(graph, 4);
+    }
 
-    static void printDFSHelper(Map<Integer,Vertex> graph, int key){
+    static void printDFSHelper(Map<Integer, Vertex> graph, int key) {
         Vertex vertex = graph.get(key);
 
-        if(vertex==null){
+        if (vertex == null) {
             return;
         }
 
-        if(vertex.isTraversed()){
+        if (vertex.isTraversed()) {
             return;
         }
 
@@ -67,7 +95,7 @@ public class WeightedGraphImplementation {
         System.out.println(vertex.getValue());
         ArrayList<Vertex> connectedNodes = vertex.getConnectedNodes();
         for (Vertex connectedNode : connectedNodes) {
-            printDFSHelper(graph,connectedNode.getValue());
+            printDFSHelper(graph, connectedNode.getValue());
         }
     }
 
